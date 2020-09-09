cae89fefee77d8fc1ba96746a312d45678831c2b
ithayaku1u <anuyanith@gmail.com>
2020-09-09 10:03:30 +0200

    Correction signes

--- GnomeJardin.java
+++ GnomeJardin.java
@@ -1,7 +1,7 @@
 public class GnomeJardin extends NainJardin  implements SuperPouvoir{
 	public int subitCharme(int coup){
-		if (this.sort()>coup){
-			return -coup;
+		if (this.sort()> -coup){
+			return coup;
 		}
 		else{
 		       	return -1;
